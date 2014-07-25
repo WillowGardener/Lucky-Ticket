a30b76a0149ebdb4fb1fc4b28b0d88a2eac206c5
Griffin Wimminger <student@epicodus.com>
2014-07-25 12:05:25 -0700

    refactor: code no longer gives you rabies; eval method not used

--- lib/luck_check.rb
+++ lib/luck_check.rb
@@ -1,10 +1,21 @@
 def luck_check(ticket)
   tick_length = ticket.length
   tick_split = -(tick_length/2)
-  tick_left = ticket.slice(0, tick_length/2).split("").join("+")
-  tick_right = ticket.slice(tick_split..-1).split("").join("+")
-  left_sum = eval(tick_left)
-  right_sum = eval (tick_right)
+
+  tick_left = ticket.slice(0, tick_length/2).split("")
+  tick_right = ticket.slice(tick_split..-1).split("")
+
+  left_sum = 0
+  tick_left.each do |digit|
+    left_sum += digit.to_i
+  end
+
+  right_sum = 0
+  tick_right.each do |digit|
+    right_sum += digit.to_i
+  end
+
+
   if left_sum == right_sum
     true
   else
--- spec/luck_check_spec.rb
+++ spec/luck_check_spec.rb
@@ -2,7 +2,10 @@ require("rspec")
 require("luck_check.rb")
 
 describe("luck_check") do
-  it("adds the numbers on two sides of a string, then compares them") do
-    expect(luck_check("56074")).to(eq(true))
+  it("adds the numbers on two sides of a string; if they are equal, returns true") do
+    expect(luck_check("56351014474")).to(eq(true))
+  end
+  it("adds the numbers on two sides of a string; if they are unequal, returns false") do
+    expect(luck_check("56054")).to(eq(false))
   end
 end
