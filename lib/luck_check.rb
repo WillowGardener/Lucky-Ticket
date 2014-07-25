1e3783fa66a0f60836f2c720b0d125d51e7ea408
Griffin Wimminger <student@epicodus.com>
2014-07-25 11:18:48 -0700

    splits string into right and left half, although tests must be modified accordingly as only one output is accepted

--- lib/luck_check.rb
+++ lib/luck_check.rb
@@ -1,4 +1,8 @@
 def luck_check(ticket)
   tick_length = ticket.length
+  tick_split = -(tick_length/2)
+  tick_left = ticket.slice(0, tick_length/2)
+  tick_right = ticket.slice(tick_split..-1)
+  tick_left
 end
 
--- spec/luck_check_spec.rb
+++ spec/luck_check_spec.rb
@@ -2,7 +2,7 @@ require("rspec")
 require("luck_check.rb")
 
 describe("luck_check") do
-  it("returns the length of a string of numbers") do
-    expect(luck_check("5674")).to(eq(4))
+  it("separates the string's left half from its right") do
+    expect(luck_check("5674")).to(eq("56"))
   end
 end
